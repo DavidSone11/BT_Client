c50615f07c815d7456958a30358937b88c9a1363
santosh <santosh_citech@live.in>
2017-10-28 16:21:36 +0530

    dfdfdfd

--- gulpfile.js
+++ gulpfile.js
@@ -53,16 +53,45 @@ gulp.task('custom-html', function () {
         .pipe(gulp.dest('public/'));
 });
 
+
+
+//To Minified JS
+gulp.task('custom-js', function () {
+    return gulp.src('public_dev/js/**/*.*')
+        .pipe(minifyJs())
+        .pipe(concat('dashboard.min.js'))
+        .pipe(gulp.dest('public/javascripts'));
+});
+
+//To Minified Images
+gulp.task('custom-images', function () {
+    return gulp.src('public_dev/images/**/*.*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('public/images'));
+});
+
+//For ng-folder
+gulp.task('custom-ng', function () {
+    console.log("ng is updating");
+    return gulp.src('public_dev/ng/**/*')
+       // .pipe(minifyHTML({ collapseWhitespace: true }))
+        .pipe(gulp.dest('public/ng'));
+});
+
 // To watch live activities 
 gulp.task('custom-watch', function () {
     gulp.watch(['public_dev/less/**/*.*'],['custom-styles']);
     gulp.watch(['public_dev/scss/**/*.*'],['custom-styles']);
     gulp.watch(['public_dev/css/**/*.*'],['custom-styles']);
     gulp.watch(['public_dev/index.html'],['custom-html']);
+    gulp.watch(['public_dev/js/**/*.*'],['custom-js']);
+    gulp.watch(['public_dev/images/**/*.*'],['custom-images']);
+    gulp.watch(['public_dev/ng/**/*'],['custom-ng']);
     gulp.watch('public_dev/**/*.*', browserSync.reload);
     
     
 });
+
 
 // Live-Sync on browser
 gulp.task('browserSync', function () {
@@ -74,5 +103,5 @@ gulp.task('browserSync', function () {
 });
 
 //To build a gulp task
-gulp.task('build', ['custom-styles', 'custom-html']);
+gulp.task('build', ['custom-styles', 'custom-html','custom-js','custom-images','custom-ng']);
 gulp.task('default', ['build', 'browserSync','custom-watch']);
