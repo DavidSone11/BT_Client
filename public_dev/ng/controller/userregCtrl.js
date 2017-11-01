674d1d0ccba64c77d37ecc82fbd3715280effc3f
santosh <santosh_citech@live.in>
2017-11-01 20:28:08 +0530

    dasdasd

--- public_dev/ng/controller/userregCtrl.js
+++ public_dev/ng/controller/userregCtrl.js
@@ -3,7 +3,7 @@
  */
 
 var app = angular.module('BTAPP');
-app.controller('userregCtrl', function ($scope, $location, toaster) {
+app.controller('userregCtrl', function ($scope, $location, toaster, $http) {
     console.log("Inside Regi Controller");
 
     $scope.roles = [
@@ -19,7 +19,7 @@ app.controller('userregCtrl', function ($scope, $location, toaster) {
         {
             name: 'User'
         }
-    ]
+    ];
 
     $scope.genders = [
         {
@@ -31,7 +31,62 @@ app.controller('userregCtrl', function ($scope, $location, toaster) {
         {
             name: 'Other'
         }
-    ]
+    ];
+
+    $scope.users = [];
+    $scope.userRegister = {};
+
+    $scope.registerUser = function () {
+
+        var regUrl = "http://localhost:4000/user/create";
+
+
+        $http.post(regUrl, $scope.userRegister, {
+
+
+        }).then(function (responseData) {
+            console.log("dasds");
+        });
+
+
+
+
+    };
+
+    $scope.getUsers = function () {
+
+        var regUrl = "http://localhost:4000/user/getUsers";
+        $http.get(regUrl).then(function (responseData) {
+            $scope.users = responseData.data.results;
+        }, function error(res) {
+
+        });
+
+    };
+
+    $scope.getUsers();
+
+
+    $scope.removeUser = function (user) {
+
+        console.log(user);
+        var id = user._id;
+        $scope.users.splice(user, 1);
+        $http.delete("http://localhost:4000/user/remove/", { params: { "id": id } }).then(function (result) {
+            console.log(result);
+
+            toaster.pop({
+                type: 'success',
+                title: 'Remove',
+                body: 'Remove User Successfully!!!!!',
+                timeout: 3000
+            });
+
+        }).error(function () {
+            console.log("error");
+        });
+
+    };
 
 
 });
